b54fbf3ad6b8337338e35ad10c815ca39e7da332
Anatol <ao@aivia.io>
2023-01-15 16:40:46 +0100

    Fix | typo in cancel algo orders

--- requests/rest/trade/trade_requests.go
+++ requests/rest/trade/trade_requests.go
@@ -112,7 +112,7 @@ type (
 	}
 	CancelAlgoOrder struct {
 		InstID string `json:"instId"`
-		AlgoID string `json:"AlgoId"`
+		AlgoID string `json:"algoId"`
 	}
 	AlgoOrderList struct {
 		InstType okex.InstrumentType `json:"instType,omitempty"`
