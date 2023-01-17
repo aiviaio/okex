b95817a6516302ec1bc46f47a03cb8c7c5394a47
Anatol <ao@aivia.io>
2023-01-17 15:16:14 +0100

    ADD | TrailingStopOrder string removed

--- models/trade/trade_models.go
+++ models/trade/trade_models.go
@@ -125,9 +125,9 @@ type (
 		TpTriggerPxType string              `json:"tpTriggerPxType"`
 		SlTriggerPxType string              `json:"slTriggerPxType"`
 		TriggerPx       okex.JSONFloat64    `json:"triggerPx"`
-		CallbackRatio   okex.JSONFloat64    `json:"callbackRatio,string"`
-		CallbackSpread  okex.JSONFloat64    `json:"callbackSpread,string"`
-		ActivePx        okex.JSONFloat64    `json:"activePx,string"`
+		CallbackRatio   okex.JSONFloat64    `json:"callbackRatio"`
+		CallbackSpread  okex.JSONFloat64    `json:"callbackSpread"`
+		ActivePx        okex.JSONFloat64    `json:"activePx"`
 		OrdPx           okex.JSONFloat64    `json:"ordPx"`
 		Fee             okex.JSONFloat64    `json:"fee"`
 		Rebate          okex.JSONFloat64    `json:"rebate"`
