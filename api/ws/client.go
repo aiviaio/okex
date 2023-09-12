de14f77768604c4bbef96f2cec000a91af0a2f47
Artem Korol <a.korol@aumify.io>
2023-09-12 21:25:04 +0200

    change to single pointer reference

--- api/ws/client.go
+++ api/ws/client.go
@@ -363,7 +363,7 @@ func (c *ClientWs) receiver(p bool) error {
 
 			if mt == websocket.TextMessage && string(data) != "pong" {
 				e := &events.Basic{}
-				if err := json.Unmarshal(data, &e); err != nil {
+				if err := json.Unmarshal(data, e); err != nil {
 					return err
 				}
 				go c.process(data, e)
